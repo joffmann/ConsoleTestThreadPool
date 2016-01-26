782f76f388ecd50ee5d2021a1d63b455d0fb5d21
Jens Hoffmann Axén <jens.hoffmann@soderbergpartners.se>
2016-01-26 16:34:23 +0100

    Add

---
+++ ConsoleTestThreadPool/ConsoleTestThreadPool/Fibonacci.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+namespace ConsoleTestThreadPool
+{
+    public class Fibonacci
+    {
+        private int _n;
+        private int _fibOfN;
+        private ManualResetEvent _doneEvent;
+
+        public int N { get { return _n; } }
+        public int FibOfN { get { return _fibOfN; } }
+
+        // Constructor.
+        public Fibonacci(int n, ManualResetEvent doneEvent)
+        {
+            _n = n;
+            _doneEvent = doneEvent;
+        }
+
+        // Wrapper method for use with thread pool.
+        public void ThreadPoolCallback(object threadContext)
+        {
+            int threadIndex = (int)threadContext;
+            Debug.WriteLine("thread {0} started...", threadIndex);
+            _fibOfN = Calculate(_n);
+            Debug.WriteLine("thread {0} result calculated...", threadIndex);
+            _doneEvent.Set();
+        }
+
+        // Recursive method that calculates the Nth Fibonacci number.
+        public int Calculate(int n)
+        {
+            if (n <= 1) {
+                return n;
+            }
+
+            return Calculate(n - 1) + Calculate(n - 2);
+        }
+    }
+
+}
