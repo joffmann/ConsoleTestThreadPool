ff6863e3e54d7808844543ad6d3815e556566a23
Jens Hoffmann Axén <jens.hoffmann@soderbergpartners.se>
2016-01-27 08:20:56 +0100

    Klar

---
+++ ConsoleTestThreadPool/ConsoleTestThreadPool/Fibonacci.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+namespace ConsoleTestThreadPool
+{
+    public class Fibonacci
+    {
+        private int _n;
+        private int _fibOfN;
+        private ManualResetEvent _doneEvent;
+
+        public int N { get { return _n; } }
+        public int FibOfN { get { return _fibOfN; } }
+
+        // Constructor.
+        public Fibonacci(int n, ManualResetEvent doneEvent) {
+            _n = n;
+            _doneEvent = doneEvent;
+        }
+
+        // Wrapper method for use with thread pool.
+        public void ThreadPoolCallback(object threadContext) {
+            Thread.Sleep(20);
+            int threadIndex = (int)threadContext;
+            Debug.WriteLine("thread {0} started...", threadIndex);
+
+            _fibOfN = Calculate(_n);
+            Debug.WriteLine("Thread:{0} Nr:{1} result calculated:{2}", threadIndex,N, _fibOfN);
+            _doneEvent.Set();
+        }
+
+        // Recursive method that calculates the Nth Fibonacci number.
+        public int Calculate(int n) {
+            if (n <= 1) 
+                return n;
+            return Calculate(n - 1) + Calculate(n - 2);
+        }
+    }
+
+}
---
+++ ConsoleTestThreadPool/ConsoleTestThreadPool/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ConsoleTestThreadPool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int FibonacciCalculations = 10;
+
+            // One event is used for each Fibonacci object.
+            ManualResetEvent[] doneEvents = new ManualResetEvent[FibonacciCalculations];
+            Fibonacci[] fibArray = new Fibonacci[FibonacciCalculations];
+            Random r = new Random();
+
+            // Configure and start threads using ThreadPool.
+            Debug.WriteLine("launching {0} tasks...", FibonacciCalculations);
+
+            for (int i = 0; i < FibonacciCalculations; i++) {
+                doneEvents[i] = new ManualResetEvent(true);
+            }
+
+            for (int i = 0; i < 100; i++) {
+                int threadNr = WaitHandle.WaitAny(doneEvents);
+                doneEvents[threadNr] = new ManualResetEvent(false);
+                Fibonacci f = new Fibonacci(r.Next(20, 40), doneEvents[threadNr]);
+                fibArray[threadNr] = f;
+                ThreadPool.QueueUserWorkItem(f.ThreadPoolCallback, threadNr);
+                Debug.WriteLine("Fibonacci({0}) Thread:{1} Sequens:{2}", f.N, threadNr,i);
+            }
+
+            // Wait for all threads in pool to calculate.
+            //WaitHandle.WaitAll(doneEvents);
+            Debug.WriteLine("All calculations are complete.");
+        }
+    }
+}
